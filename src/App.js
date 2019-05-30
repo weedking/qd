863dad37c2304c4d4ed83a80094dbc68bdbf000b
weedking <260815998@qq.com>
2019-05-31 06:31:14 +0800

    react-router-dom完整例子

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Header from './Header'
+import Main from './Main'
+
+const App = () => (
+    <div>
+      <Header />
+      <Main />
+    </div>
+)
+
+export default App;
